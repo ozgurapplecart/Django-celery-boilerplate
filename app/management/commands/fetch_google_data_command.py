fe841de0c0d482c6b32dd8778c441c54d0bd1189
ozgurgundogan <ozgur@applecart.co>
2023-07-07 15:17:00 -0400

    like this to be added

---
+++ app/management/commands/fetch_google_data_command.py
@@ -0,0 +1,18 @@
+from datetime import timedelta, time, datetime
+from django.core.management import BaseCommand
+from django.utils import timezone
+from django.utils.timezone import make_aware
+from app.google_fetcher.google_fetcher import GoogleDataGetter
+
+today = timezone.now()
+tomorrow = today + timedelta(1)
+today_start = make_aware(datetime.combine(today, time()))
+today_end = make_aware(datetime.combine(tomorrow, time()))
+
+
+class Command(BaseCommand):
+    help = "Send Today's Orders Report to Admins"
+
+    def handle(self, *args, **options):
+        print('dsds')
+        return 1
